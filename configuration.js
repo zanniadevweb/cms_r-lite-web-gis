764509c1d8914b43526a985ebf13dd595dd7c469
Alexandre Zanni <zanni.a.dev.web@gmail.com>
2024-07-10 22:40:55 +0200

    feat: template polygons roman empire

--- configuration.js
+++ configuration.js
@@ -1,3 +1,31 @@
 // ------------------- VARIABLES GLOBALES -------------------
 startingViewLatLng = [40.421190, 15.005673];
 startingViewZoom = 4;
+
+customMapLegend = 
+'<label id="checkboxRegionLabelState" class="input-check"><input onchange="change_state(this)" onclick="changeRegionLabelState()" type="checkbox" style="font-size:15px"/> <span id="textRegionLabelState">- OFF LABELS REGION -</span></label><br>'
++'<button id="noRegions" onclick="noRegions()" style="background: #FFA500; font-size:15px">NO REGIONS AREAS</button><br>'
++'<button id="withRegions" onclick="withRegions()" style="background: #50C878; font-size:15px">WITH REGIONS AREAS</button><br>'
++'<button id="romanFrontiersFirstCentury" onclick="romanFrontiersFirstCentury()" style="background: #d93616; font-size:15px">ROMAN FRONTIERS Ist c. B.C.</button><br><br>'
++'<h4>Map Legend</h4><br>';
++'<i style="background: #808080"></i><span>Mixed or Unkown Datation</span><br>';
++'<i style="background: #00FF00"></i><span>V<sup>th</sup> c. B.C.</span><br>';
++'<i style="background: #088F8F"></i><span>IV<sup>th</sup> c. B.C.</span><br>';
++'<i style="background: #0000FF"></i><span>III<sup>rd</sup> c. B.C.</span><br>';
++'<i style="background: #FF0000"></i><span>II<sup>nd</sup> c. B.C.</span><br>';
++'<i style="background: #FAA0A0"></i><span>I<sup>st</sup> c. B.C. (or later)</span><br>';
+
+disablePolygonsFirstLoadingMap = false;
+
+function romanFrontiersFirstCentury() {
+    noRegions();
+    var elements = document.querySelectorAll('.roman_republic_cyprus, .roman_republic_cyclades, .roman_republic_crete, .roman_republic_egypt, .roman_republic_sicily, .roman_republic_carthago, .roman_republic_sardinia, .roman_republic_spain, .roman_republic_italy, .roman_republic_illyria, .roman_republic_epirus, .roman_republic_achean_league, .roman_republic_etolian_league, .roman_republic_phenicia, .roman_republic_attalid_kingdom');
+    for (var iElements = 0; iElements < elements.length ; iElements++) {
+        elements[iElements].style.display = '';
+    }
+    if (document.getElementById('checkboxRegionLabelState').classList.contains('checked')) {
+        noLabels();
+    } else {
+        withLabels();
+    }
+}
